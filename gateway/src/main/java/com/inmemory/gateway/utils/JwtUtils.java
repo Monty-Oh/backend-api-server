b098b398877a1de42ec9cc90528e85b4b00ff3f4
오한진 <monty@plgrim.com>
2024-03-05 16:57:23 +0900

    JWT 검증 추가 중

--- gateway/src/main/java/com/inmemory/gateway/utils/JwtUtils.java
+++ gateway/src/main/java/com/inmemory/gateway/utils/JwtUtils.java
@@ -1,18 +1,29 @@
 package com.inmemory.gateway.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
-import java.time.LocalDateTime;
-import java.util.Date;
+import javax.crypto.SecretKey;
 
 @Component
 public class JwtUtils {
 
-    //  TODO: 시크릿 키는 별도로 보관할 것.
-    private static final String JWT_SECRET_KEY = "2cb6363001584512e547b1e327a0404dd48ba9e2452b277c0d0bb4aec3024aff5a82fe17c50e5ed573223b7139cd476252d6082673c33fe1677be106d5223252";
+    private final SecretKey jwtSecretKey;
 
-    public String createToken(int id) {
-        LocalDateTime now = LocalDateTime.now();
+    public JwtUtils(@Value("${service.jwt.secret-key}") String secretKey) {
+        this.jwtSecretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    }
+
+    public void validateToken(String accessToken) {
+        Jws<Claims> claimsJws = Jwts.parser()
+                .verifyWith(jwtSecretKey)
+                .build()
+                .parseSignedClaims(accessToken);
+        Claims payload = claimsJws.getPayload();
     }
 }
