3ac8308356f73bd9e74a84ff7936601787ccfa94
오한진 <monty@plgrim.com>
2024-03-08 15:29:26 +0900

    GlobalExceptionHandler 추가

--- gateway/src/main/java/com/inmemory/gateway/common/constants/StaticValue.java
+++ gateway/src/main/java/com/inmemory/gateway/common/constants/StaticValue.java
@@ -5,4 +5,8 @@ public class StaticValue {
     public static final String HEADER_ACCESS_TOKEN = "access_token";
 
     public static final String HEADER_REFRESH_TOKEN = "refresh_token";
+
+    public static final String HEADER_RESPONSE_CODE = "errorCode";
+
+    public static final String HEADER_RESPONSE_MESSAGE = "message";
 }
---
+++ gateway/src/main/java/com/inmemory/gateway/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.inmemory.gateway.common.handler;
+
+import com.inmemory.gateway.common.exception.ApplicationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebExceptionHandler;
+import reactor.core.publisher.Mono;
+
+import static com.inmemory.gateway.common.constants.ErrorCode.INTERNAL_SERVER_ERROR;
+import static com.inmemory.gateway.common.constants.StaticValue.HEADER_RESPONSE_CODE;
+import static com.inmemory.gateway.common.constants.StaticValue.HEADER_RESPONSE_MESSAGE;
+
+@Order(-2)
+@Slf4j
+@Component
+public class GlobalExceptionHandler implements WebExceptionHandler {
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+        ServerHttpResponse serverHttpResponse = exchange.getResponse();
+        HttpHeaders serverHttpResponseHeaders = serverHttpResponse.getHeaders();
+
+        if (ex instanceof ApplicationException applicationException) {
+            serverHttpResponseHeaders.add(HEADER_RESPONSE_CODE, applicationException.getErrorCode());
+            serverHttpResponseHeaders.add(HEADER_RESPONSE_MESSAGE, applicationException.getMessage());
+            serverHttpResponse.setStatusCode(applicationException.getHttpStatus());
+        } else {
+            serverHttpResponseHeaders.add(HEADER_RESPONSE_CODE, INTERNAL_SERVER_ERROR.getCode());
+            serverHttpResponseHeaders.add(HEADER_RESPONSE_MESSAGE, INTERNAL_SERVER_ERROR.getMessage());
+            serverHttpResponse.setStatusCode(INTERNAL_SERVER_ERROR.getHttpStatus());
+        }
+
+        return serverHttpResponse.setComplete();
+    }
+}
