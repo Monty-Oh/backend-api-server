26977c27721c52c3bc49faf30fe394bb5d8bdd85
ohhanjin <ohhanjin0330@gmail.com>
2024-03-25 21:46:17 +0900

    User 도메인 - FeignClient Unit Test 추가 중

--- user/src/test/java/com/inmemory/user/infrastructure/feign/AuthFeignClientTest.java
+++ user/src/test/java/com/inmemory/user/infrastructure/feign/AuthFeignClientTest.java
@@ -1,15 +1,35 @@
 package com.inmemory.user.infrastructure.feign;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.inmemory.user.infrastructure.feign.dto.AuthCreateTokenReqDto;
+import com.inmemory.user.infrastructure.feign.dto.AuthCreateTokenRspDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 
 class AuthFeignClientTest extends WireMockTest{
 
-    @Autowired
-    private WireMockServer mockServer;
+    private final WireMockServer wireMockServer = new WireMockServer();
 
     @Autowired
     private AuthFeignClient authFeignClient;
 
+    @Test
+    @DisplayName("test...")
+    void createAccessTokenAndRefreshToken_success() throws JsonProcessingException {
+        AuthCreateTokenRspDto authCreateTokenRspDto = AuthCreateTokenRspDto.builder()
+                .accessToken("testAccessToken")
+                .refreshToken("testRefreshToken")
+                .build();
+        setupPostSuccessResponse(wireMockServer, authCreateTokenRspDto);
 
+        ResponseEntity<AuthCreateTokenRspDto> accessTokenAndRefreshToken = authFeignClient.createAccessTokenAndRefreshToken(
+                AuthCreateTokenReqDto.builder().build()
+        );
+
+        System.out.println("accessTokenAndRefreshToken = " + accessTokenAndRefreshToken);
+
+    }
 }
--- user/src/test/java/com/inmemory/user/infrastructure/feign/WireMockTest.java
+++ user/src/test/java/com/inmemory/user/infrastructure/feign/WireMockTest.java
@@ -1,9 +1,27 @@
 package com.inmemory.user.infrastructure.feign;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWireMock(port = 0, stubs = "classpath:/stubs/**/*.json")
+@AutoConfigureWireMock
 public class WireMockTest {
+    @Autowired
+    protected ObjectMapper mapper;
+
+    protected void setupPostSuccessResponse(WireMockServer wireMockServer, Object responseBody) throws JsonProcessingException {
+        wireMockServer.stubFor(
+                WireMock.post(WireMock.anyUrl())
+                        .willReturn(WireMock.aResponse()
+                                .withStatus(200)
+                                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                                .withBody(mapper.writeValueAsString(responseBody)))
+        );
+    }
 }
