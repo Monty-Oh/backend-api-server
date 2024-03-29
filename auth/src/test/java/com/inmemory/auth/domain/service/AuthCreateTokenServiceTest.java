128cdd828f78cde8ef1d8f2470083cdd79f9aef7
오한진 <monty@plgrim.com>
2024-03-29 16:49:28 +0900

    Auth 도메인  - 유닛 테스트 추가 중

--- auth/src/test/java/com/inmemory/auth/domain/service/AuthCreateTokenServiceTest.java
+++ auth/src/test/java/com/inmemory/auth/domain/service/AuthCreateTokenServiceTest.java
@@ -1,7 +1,24 @@
 package com.inmemory.auth.domain.service;
 
+import com.inmemory.auth.common.utils.JwtUtils;
+import com.inmemory.auth.domain.repository.AuthRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class AuthCreateTokenServiceTest {
+
+    @InjectMocks
+    private AuthCreateTokenService authCreateTokenService;
+
+    @Mock
+    private AuthRepository authRepository;
+    @Mock
+    private JwtUtils jwtUtils;
+
 
 }
