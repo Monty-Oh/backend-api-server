280982886c4a136162118676569c40df66177c82
ohhanjin <ohhanjin0330@gmail.com>
2024-07-17 21:17:25 +0900

    web  vite -> vue-cli 수정 중

--- web_vue/src/components/common/Footer.vue
+++ web_vue/src/components/common/Footer.vue
@@ -5,4 +5,9 @@
       <a href="https://github.com/Monty-Oh" target="_blank">who made this?</a>
     </h5>
   </footer>
+  <v-app>
+    <v-content>
+      <v-container>Hello world</v-container>
+    </v-content>
+  </v-app>
 </template>
--- web_vue/src/main.js
+++ web_vue/src/main.js
@@ -1,9 +1,12 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import store from './store'
+import Vuetify from 'vuetify';
+
 
 //  Vue Application Setting
 createApp(App)
     .use(store)
+    .use(Vuetify)
     .mount('#app')
 ;
