09b027bab276ee80b5279a012ae8daafcc713c09
오한진 <monty@plgrim.com>
2024-10-23 08:55:25 +0900

    IntegrationTest  - 로그인 요청과 성공 응답 테스트 추가

--- user/src/test/java/com/inmemory/user/integrationTest/UserControllerTest.java
+++ user/src/test/java/com/inmemory/user/integrationTest/UserControllerTest.java
@@ -1,10 +1,20 @@
 package com.inmemory.user.integrationTest;
 
+import com.inmemory.user.interfaces.rest.constants.UserApiUrl;
 import com.inmemory.user.interfaces.rest.dto.UserLoginReqDto;
+import com.inmemory.user.interfaces.rest.dto.UserLoginRspDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserControllerTest extends WireMockServerTest {
 
@@ -12,16 +22,26 @@ public class UserControllerTest extends WireMockServerTest {
     @DisplayName("로그인 요청을 했고 성공 응답을 받는다.")
     void login_success() {
         //  given
+        String userNo = "0000";
+        String loginId = "testLoginId";
+        String password = "testPassword";
+        this.insertUserData(userNo, loginId, password);
         UserLoginReqDto userLoginReqDto = UserLoginReqDto.builder()
-                .loginId("testLoginId")
-                .password("testPassword")
+                .loginId(loginId)
+                .password(password)
                 .build();
+        HttpEntity<UserLoginReqDto> requestHttpEntity = new HttpEntity<>(userLoginReqDto);
 
-        HttpEntity<UserLoginReqDto> httpEntity = new HttpEntity<>(userLoginReqDto);
-        ResponseEntity<String> responseEntity =
-                restTemplate.postForEntity("/user/login", httpEntity, String.class);
+        //  when
+        ResponseEntity<UserLoginRspDto> responseEntity = restTemplate.postForEntity(UserApiUrl.USER_V1_BASE_URL + UserApiUrl.LOGIN_URL, requestHttpEntity, UserLoginRspDto.class);
+        Optional<UserLoginRspDto> actual = Optional.ofNullable(responseEntity.getBody());
 
-        System.out.println("responseEntity = " + responseEntity);
-
+        //  then
+        assertAll(
+                () -> assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK),
+                () -> assertTrue(actual.isPresent()),
+                () -> assertTrue(actual.map(UserLoginRspDto::getAccessToken).isPresent()),
+                () -> assertTrue(actual.map(UserLoginRspDto::getRefreshToken).isPresent())
+        );
     }
 }
--- user/src/test/java/com/inmemory/user/integrationTest/WireMockServerTest.java
+++ user/src/test/java/com/inmemory/user/integrationTest/WireMockServerTest.java
@@ -1,5 +1,8 @@
 package com.inmemory.user.integrationTest;
 
+import com.inmemory.user.common.utils.EncryptUtil;
+import com.inmemory.user.domain.model.aggregate.User;
+import com.inmemory.user.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -9,11 +12,25 @@ import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
  * IntegrationTest 를 위한 공통 WireMockServer 세팅 클래스
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWireMock(port = 0, stubs = "classpath:/stubs/**/*.json")
+@AutoConfigureWireMock(port = 8080, stubs = "classpath:/stubs/**/*.json")
 public class WireMockServerTest {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     protected TestRestTemplate restTemplate;
 
     //  TODO: 공통 헤더 세팅 ...
+
+    //  TODO: DB에 데이터 미리 삽입
+    protected void insertUserData(String userNo, String loginId, String password) {
+        String encryptedPassword = EncryptUtil.encode(password);
+        User user = User.builder()
+                .userNo(userNo)
+                .loginId(loginId)
+                .password(encryptedPassword)
+                .build();
+        userRepository.save(user);
+    }
 }
