21dffc9c3d952156f0725deee19f04c3f353b6c9
ohhanjin <ohhanjin0330@gmail.com>
2024-10-26 22:40:05 +0900

    IntegrationTest  - 토큰 생성 관련 테스트 추가

--- auth/src/main/java/com/inmemory/auth/domain/repository/UserRoleRepository.java
+++ auth/src/main/java/com/inmemory/auth/domain/repository/UserRoleRepository.java
@@ -3,8 +3,11 @@ package com.inmemory.auth.domain.repository;
 import com.inmemory.auth.domain.model.entity.UserRole;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRoleRepository {
 
     List<UserRole> findByUserRoleIdUserNo(String userNo);
+
+    UserRole save(UserRole userRole);
 }
