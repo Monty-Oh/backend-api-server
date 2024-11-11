d471a5eb0ab723bf606c660a77c92b0736e62e5d
ohhanjin <ohhanjin0330@gmail.com>
2024-11-12 08:46:32 +0900

    Auth  - RedisTemplate Configuration

---
+++ auth/src/main/java/com/inmemory/auth/common/configuration/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.inmemory.auth.common.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@EnableCaching
+@Configuration
+@Slf4j
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setDefaultSerializer(StringRedisSerializer.UTF_8);
+        template.afterPropertiesSet();
+
+        return template;
+    }
+}
