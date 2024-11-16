9e1b05ff3f168c0015db00f0788a344f751be06b
ohhanjin <ohhanjin0330@gmail.com>
2024-11-16 22:00:14 +0900

    Auth  - import 최적화

--- auth/src/test/java/com/inmemory/auth/domain/repository/AuthRepositoryTestConfig.java
+++ auth/src/test/java/com/inmemory/auth/domain/repository/AuthRepositoryTestConfig.java
@@ -2,14 +2,8 @@ package com.inmemory.auth.domain.repository;
 
 import com.inmemory.auth.infrastructure.repository.AuthRepositoryImpl;
 import com.inmemory.auth.infrastructure.repository.redis.AuthRedisRepository;
-import jakarta.annotation.PreDestroy;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import redis.embedded.RedisServer;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
 
 @TestConfiguration
 public class AuthRepositoryTestConfig {
